f68aae530a4f30fbbaf0db7958defd7421d3c186
Sergei Sergeev <sergeev.srg@gmail.com>
2018-06-09 07:22:21 +0300

    Added check if modules installed

--- PnPPowerShell/init-pnp.ts
+++ PnPPowerShell/init-pnp.ts
@@ -1,4 +1,6 @@
 export function initPnP(contents: string[]): void {
-    contents.push('Install-PackageProvider -Name NuGet -Force -Scope CurrentUser')
-    contents.push('Install-Module SharePointPnPPowerShellOnline -Force -Verbose -Scope CurrentUser')
+    contents.push('$nugetProvider = Get-PackageProvider -Name "NuGet" -ErrorAction SilentlyContinue')
+    contents.push('if($nugetProvider -eq $null) { Install-PackageProvider -Name NuGet -Force -Scope CurrentUser }')
+    contents.push('$pnpModule = Get-InstalledModule -Name "SharePointPnPPowerShellOnline" -ErrorAction SilentlyContinue')
+    contents.push('if($pnpModule -eq $null) { Install-Module SharePointPnPPowerShellOnline -Force -Scope CurrentUser }')
 }
